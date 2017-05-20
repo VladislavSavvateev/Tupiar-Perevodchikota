c340711042328a4602b4f3a0f2f6e55bf99e269b
VladislavSavvateev <savvateevvlad@mail.ru>
2017-05-20 12:57:11 +0900

    Реализована поддержка словарей.

--- Tupiar Perevodchikota/Form1.cs
+++ Tupiar Perevodchikota/Form1.cs
@@ -7,14 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tupiar_Perevodchikota.Dictionaries;
 
 namespace Tupiar_Perevodchikota {
 	public partial class Form1 : Form {
 		TupiarEngine te;
+		RussianToChangedRussian r_crDictionary;
 
 		public Form1() {
 			InitializeComponent();
 			te = new TupiarEngine();
+			r_crDictionary = new RussianToChangedRussian();
+			te.SetDictionary(r_crDictionary);
 		}
 
 		private void txtSource_TextChanged(object sender, EventArgs e) {
--- Tupiar Perevodchikota/TupiarEngine.cs
+++ Tupiar Perevodchikota/TupiarEngine.cs
@@ -8,31 +8,6 @@ namespace Tupiar_Perevodchikota {
 	class TupiarEngine {
 		Dictionary<String, String> pairs;
 
-		public TupiarEngine() {
-			pairs = new Dictionary<string, string>();
-			pairs.Add("б", "шесть");
-			pairs.Add("в", "б");
-			pairs.Add("г", "л");
-            pairs.Add("д", "дельта");
-			pairs.Add("ж", "большеилименьше");
-			pairs.Add("и", "н");
-			pairs.Add("л", "слешобрслеш");
-			pairs.Add("н", "х");
-			pairs.Add("п", "пи");
-			pairs.Add("р", "п");
-			pairs.Add("с", "ц");
-			pairs.Add("ф", "меньшеилибольше");
-			pairs.Add("х", "икс");
-			pairs.Add("ц", "узапятэ");
-			pairs.Add("ч", "четыре");
-			pairs.Add("ш", "илиподчёркилиподчёркили");
-			pairs.Add("щ", "илиподчёркилиподчёркилизапятэ");
-			pairs.Add("ъ", "кавычкаб");
-			pairs.Add("ы", "били");
-			pairs.Add("ь", "б");
-			pairs.Add("ю", "илитирео");
-			pairs.Add("я", "р");
-		}
 		public String Convert(String source) {
 			String result = "";
 			foreach (char ch in source.ToLower()) {
@@ -41,6 +16,10 @@ namespace Tupiar_Perevodchikota {
 				else result += str;
 			}
 			return result;
+		}
+
+		public void SetDictionary(DictionaryClass dc) {
+			if (dc.GetDictionary() != null) pairs = dc.GetDictionary();
 		}
 	}
 }
