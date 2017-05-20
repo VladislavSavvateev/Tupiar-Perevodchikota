9376c4f8af16dc77a22390af37ee88ab9182c1ae
VladislavSavvateev <savvateevvlad@mail.ru>
2017-05-20 12:28:28 +0900

    Добавьте файлы проекта.

---
+++ Tupiar Perevodchikota/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tupiar_Perevodchikota {
+	public partial class Form1 : Form {
+		TupiarEngine te;
+
+		public Form1() {
+			InitializeComponent();
+			te = new TupiarEngine();
+		}
+
+		private void txtSource_TextChanged(object sender, EventArgs e) {
+			txtDestination.Text = te.Convert(txtSource.Text);
+		}
+	}
+}
---
+++ Tupiar Perevodchikota/TupiarEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tupiar_Perevodchikota {
+	class TupiarEngine {
+		Dictionary<String, String> pairs;
+
+		public TupiarEngine() {
+			pairs = new Dictionary<string, string>();
+			pairs.Add("б", "шесть");
+			pairs.Add("в", "б");
+			pairs.Add("г", "л");
+            pairs.Add("д", "дельта");
+			pairs.Add("ж", "большеилименьше");
+			pairs.Add("и", "н");
+			pairs.Add("л", "слешобрслеш");
+			pairs.Add("н", "х");
+			pairs.Add("п", "пи");
+			pairs.Add("р", "п");
+			pairs.Add("с", "ц");
+			pairs.Add("ф", "меньшеилибольше");
+			pairs.Add("х", "икс");
+			pairs.Add("ц", "узапятэ");
+			pairs.Add("ч", "четыре");
+			pairs.Add("ш", "илиподчёркилиподчёркили");
+			pairs.Add("щ", "илиподчёркилиподчёркилизапятэ");
+			pairs.Add("ъ", "кавычкаб");
+			pairs.Add("ы", "били");
+			pairs.Add("ь", "б");
+			pairs.Add("ю", "илитирео");
+			pairs.Add("я", "р");
+		}
+		public String Convert(String source) {
+			String result = "";
+			foreach (char ch in source.ToLower()) {
+				string str = ch.ToString();
+				if (pairs.ContainsKey(str)) result += pairs[str];
+				else result += str;
+			}
+			return result;
+		}
+	}
+}
