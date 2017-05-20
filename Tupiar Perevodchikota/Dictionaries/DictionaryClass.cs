2368a9c335859e6cd3a8023f6b4f4e8c76650a01
VladislavSavvateev <savvateevvlad@mail.ru>
2017-05-20 13:08:41 +0900

    Добавлена документация. Теперь при инициализации движка словарь пуст.

--- Tupiar Perevodchikota/Dictionaries/DictionaryClass.cs
+++ Tupiar Perevodchikota/Dictionaries/DictionaryClass.cs
@@ -5,7 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Tupiar_Perevodchikota {
+
+	/// <summary>
+	/// Абстрактный класс для хранения словаря.
+	/// </summary>
 	abstract class DictionaryClass {
+
+		/// <summary>
+		/// Возвращает словарь.
+		/// </summary>
+		/// <returns>Словарь.</returns>
 		abstract public Dictionary<String, String> GetDictionary();
 	}
 }
--- Tupiar Perevodchikota/TupiarEngine.cs
+++ Tupiar Perevodchikota/TupiarEngine.cs
@@ -5,9 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Tupiar_Perevodchikota {
+
+	/// <summary>
+	/// Класс, реализующий конвертирование по принципу замены символов на другие.
+	/// </summary>
 	class TupiarEngine {
 		Dictionary<String, String> pairs;
 
+		/// <summary>
+		/// Стандартный конструктор. Словарь по умолчанию пуст.
+		/// </summary>
+		public TupiarEngine() {
+			pairs = new Dictionary<string, string>();
+		}
+
+		/// <summary>
+		/// Выполняет процедуру конвертации.
+		/// </summary>
+		/// <param name="source">Исходная строка.</param>
+		/// <returns>Преобразованная строка.</returns>
 		public String Convert(String source) {
 			String result = "";
 			foreach (char ch in source) {
@@ -21,6 +37,10 @@ namespace Tupiar_Perevodchikota {
 			return result;
 		}
 
+		/// <summary>
+		/// Устанавливает словарь.
+		/// </summary>
+		/// <param name="dc">Класс-хранилище для словаря.</param>
 		public void SetDictionary(DictionaryClass dc) {
 			if (dc.GetDictionary() != null) pairs = dc.GetDictionary();
 		}
